7e13b296fc55198fb8283c1c8cc32a45b25b0940
That_Guy <eghove@gmail.com>
2018-12-19 16:52:46 -0600

    fixing up server.js

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ let path = require("path");
 // Sets up the Express App
 // =============================================================
 let app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// variable for the data storage
-let friendUserData =[];
-
-
-
-
-
-
+// ROUTING
+// =============================================================
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 // Starts the server to begin listening
