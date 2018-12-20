fcf5cd8e05a705f13ccc19cb425af82d3d0c74cc
That_Guy <eghove@gmail.com>
2018-12-20 16:39:11 -0600

    post that pushes survey answers to friends.js is done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,9 +6,16 @@ let friendArray = require("../data/friends");
 // ===============================================================================
 
 module.exports = function(app){
-    // for the get request for the API raw data
+    // for the GET request for the API raw data
     app.get("/api/friends", function(req, res){
         res.json(friendArray);
+    });
+
+    //for the POST request to add survey data to friendArray, also the camparison logic
+    app.post("/api/friends", function(req, res) {
+        //push the survey data to friendArray
+        friendArray.push(req.body);
+        res.send(true);
     });
 
 
