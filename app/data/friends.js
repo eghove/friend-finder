d484bc5a57cca3d1941785bacd2ac31059783e0b
That_Guy <eghove@gmail.com>
2018-12-19 18:15:34 -0600

    starting to build API routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,5 @@
+// where the data from the friend finder app resides
 
+let friendArray = [];
+
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,3 @@
+// DEPENDENCIES
+
+let friendArray = require("../data/friends")
