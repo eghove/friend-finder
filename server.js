633147f7b4b7a993c05322ea31de0e350e6d3838
That_Guy <eghove@gmail.com>
2019-01-27 18:35:45 -0600

    linted server.js to standard

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
+/* eslint-disable semi */
 // Dependencies
 // =============================================================
-let express = require("express");
-
-
-
+var express = require('express');
 
 // Sets up the Express App
 // =============================================================
-let app = express();
-const PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = process.env.PORT || 8080;
 
 // MIDDLEWARE
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // ROUTING
 // =============================================================
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
-
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log('App listening on PORT ' + PORT);
+});
