7170b27b8d18f4dfe30584a13fc1194085b74a62
That_Guy <eghove@gmail.com>
2019-01-27 18:43:11 -0600

    linting htmlRoutes.js to standard

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,26 +1,25 @@
+/* eslint-disable semi */
 
 // DEPENDENCIES
-let path = require("path");
-
+var path = require('path');
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
+// directs to the survey.html
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+  });
 
-    // directs to the survey.html
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
+  // directs to home.html
+  app.get('/home', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
 
-    // directs to home.html
-    app.get("/home", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-
-    // If no matching route is found default to home.html
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+  // If no matching route is found default to home.html
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
 };
