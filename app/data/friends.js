f86ce086d2704bf75d84896f919474022222888b
That_Guy <eghove@gmail.com>
2019-01-27 18:37:15 -0600

    linted friends.js to standard

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,24 @@
+/* eslint-disable semi */
 // where the data from the friend finder app resides
 
-let friendArray = [
-    // Initial dummy data
-    {
-        name: "Eric Hove",
-        photo: "https://eghove.github.io/img/Eric_Headshot.jpg",
-        scores: [
-            "5",
-            "5",
-            "5",
-            "5",
-            "3",
-            "4",
-            "4",
-            "1",
-            "4",
-            "1"
-        ]
-    }
+var friendArray = [
+  // Initial dummy data
+  {
+    name: 'Eric Hove',
+    photo: 'https://eghove.github.io/img/Eric_Headshot.jpg',
+    scores: [
+      '5',
+      '5',
+      '5',
+      '5',
+      '3',
+      '4',
+      '4',
+      '1',
+      '4',
+      '1'
+    ]
+  }
 
 ];
 // export the data so it can be used elsewhere
